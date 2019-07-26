2e6a3bb9abbe89e73d5ba4b4267cc1f564cba0be
blasdch18 <blas.cruz@ucsp.edu.pe>
2019-07-25 22:20:34 -0500

    First Bootstrap Steps

---
+++ LogSys/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+	<meta charset="utf-8">
+	<title>Login</title>
+
+	<!-- IMPORT BOOTSTRAP CSS FRAMEWORK -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+	<!-- IMPORT BOOTSTRAP JS FRAMEWORK -->
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+
+	
+
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 well" >
+				<h1>Log</h1>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
