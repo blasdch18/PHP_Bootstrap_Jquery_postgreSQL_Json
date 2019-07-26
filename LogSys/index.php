6c027f874ac6e18f4ae3765484f16b2af16c7945
blasdch18 <blas.cruz@ucsp.edu.pe>
2019-07-26 00:13:27 -0500

    merging

--- LogSys/index.php
+++ LogSys/index.php
@@ -1,29 +1,49 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
 
-	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Login</title>
 
 	<!-- IMPORT BOOTSTRAP CSS FRAMEWORK -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 
 	<!-- IMPORT BOOTSTRAP JS FRAMEWORK -->
 	<script type="text/javascript" src="js/bootstrap.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 
-
-	
-
 </head>
+
 <body>
+
+	<div class="navbar navbar-inverse navbar-fixed-top" rol="navigation">
+		<div class="navbar-collapse collapse">
+			<div class="container">
+				<ul class="nav navbar-nav">
+					<li>
+						<a href="#">Home <b class="caret"></b></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+
+
+
 	<div class="container">
 		<div class="row">
+			<div class="jumbotron">
+				<h1>Jumbotron</h1>		
+				<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+				<center><br>
+				<button class="btn btn-lg btn-success"> Sign Up</button></center>
+			</div>
 			<div class="col-lg-12 well" >
-				<h1>Log</h1>
+				<h1>My First Logging!!.......</h1>
 			</div>
 		</div>
 	</div>
 
 </body>
+
 </html>
